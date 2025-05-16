23eb7016da972dd22abafbd7b08e16bb5338bf73
mcmoodoo <rashid.makhmud@gmail.com>
2025-05-16 09:06:31 -0400

    updated config

--- tf/main.tf
+++ tf/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.2"
+  version = "5.21.0"
 
   name = "eks-vpc"
   cidr = "10.0.0.0/16"
@@ -19,22 +19,23 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.4"
+  version = "~> 20.8.4"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.29"
+  cluster_version = "1.32"
   subnet_ids      = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
-  cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_public_access_cidrs = ["98.109.114.2/32"]
 
   eks_managed_node_groups = {
     default = {
-      instance_types = ["t3.medium"]
-      min_size       = 1
+      instance_types = ["t3.small"]
+      min_size       = 2
       max_size       = 2
-      desired_size   = 1
+      desired_size   = 2
     }
   }
 
--- tf/variables.tf
+++ tf/variables.tf
@@ -3,5 +3,5 @@ variable "region" {
 }
 
 variable "cluster_name" {
-  default = "demo-cluster"
+  default = "mcmoodoo-cluster"
 }
