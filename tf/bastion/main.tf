e863528c8821bb929f9f71d57a04bbdeafe33535
mcmoodoo <rashid.makhmud@gmail.com>
2025-06-30 09:38:09 -0400

    removed hard-coded region value

--- tf/bastion/main.tf
+++ tf/bastion/main.tf
@@ -86,7 +86,7 @@ terraform {
   backend "s3" {
     bucket         = "mcmoodoo-terraform-state-bucket"
     key            = "bastion/terraform.tfstate"
-    region         = "us-east-1"
+    region         = var.region
     encrypt        = true
     dynamodb_table = "terraform-locks" # optional but useful
   }
